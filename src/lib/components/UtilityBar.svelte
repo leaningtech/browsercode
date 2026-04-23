3c093fa81f409b8fad8cc95692764a5f2df365f4
Gracjan <barylagracjan@icloud.com>
2026-04-23 14:49:20 +0100

    feat: added contents to utilitybar

--- src/lib/components/UtilityBar.svelte
+++ src/lib/components/UtilityBar.svelte
@@ -1,1 +1,21 @@
-<footer class="h-10 border-t border-white/10"></footer>
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+</script>
+
+<footer
+	class="flex h-10 items-center justify-between border-t border-white/10 px-4 text-xs text-gray-500"
+>
+	<div class="flex items-center gap-2">
+		<span>Made with Browserpod v2.2.1</span>
+	</div>
+	<div class="flex items-center gap-2">
+		<a
+			href="https://browserpod.io/blog/browserpod-2-0/"
+			target="_blank"
+			class="flex items-center gap-1 hover:text-gray-300"
+		>
+			<span>Latest announcement: Git, bash, BrowserPod. 2.0 is here</span>
+			<Icon icon="mingcute:arrow-right-up-line" />
+		</a>
+	</div>
+</footer>
