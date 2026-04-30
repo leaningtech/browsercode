bf33fb41e13e2409d293b4994c99671e3178bf56
Gracjan <barylagracjan@icloud.com>
2026-04-30 13:20:26 +0100

    feat: add iframe CSP headers to allow browserpod.io to embed the demo

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,5 +4,6 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
 	response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
 	response.headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
+	response.headers.set('Content-Security-Policy', "frame-ancestors 'self' https://browserpod.io");
 	return response;
 };
--- src/lib/components/Terminal.svelte
+++ src/lib/components/Terminal.svelte
@@ -1,3 +1,3 @@
 <div class="flex h-full min-h-0 w-full min-w-0 flex-col bg-[#0d0d0e]">
-	<div id="console" class="min-h-0 flex-1 bg-black"></div>
+	<div id="console" class="min-h-0 flex-1 bg-black p-4"></div>
 </div>
